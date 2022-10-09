4c74c22e55f4549af7861b33bc4daac60f7dbe6f
Salihah Alnahdi <salihah.alnahdi@kcl.ac.uk>
2022-10-10 00:10:43 +0100

    Test Assertion

--- microblogs/models.py
+++ microblogs/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-# Create your models here.
+from django.core.validators import RegexValidator
+
 class User(AbstractUser):
+    username = models.CharField(max_length=30,unique = True, 
+    validators=[RegexValidator(
+        regex = r'^@\w{3,}$',
+        message = 'Username must consist of @ followed by at least three alphanumericals'
+    )])
+
     bio = models.TextField()
--- microblogs/tests.py
+++ microblogs/tests.py
@@ -1,3 +1,68 @@
+from django.core.exceptions import ValidationError
 from django.test import TestCase
+from microblogs.models import User
 
-# Create your tests here.
+class UserModelTestCase(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user('@johndoe',
+            first_name = 'John',
+            last_name = 'Doe',
+            email = 'johndoe@gmail.com',
+            password = 'Password123',
+            bio = 'Hi, I am John.')
+
+    def test_valid_user(self):
+        self._assert_user_is_valid()
+    
+    def test_username_cannot_be_blank(self):
+        self.user.username = ''
+        self._assert_user_is_invalid()
+    
+    def test_username_can_be_30_characters_long(self):
+        self.user.username = '@' + 'x' * 29
+        self._assert_user_is_valid()
+    
+    def test_username_cannot_be_over_30_characters_long(self):
+        self.user.username = '@' + 'x' * 30
+        self._assert_user_is_invalid()
+    
+    def test_username_must_be_unique(self):
+        User.objects.create_user('@janedoe',
+            first_name = 'Jane',
+            last_name = 'Doe',
+            email = 'janedoe@gmail.com',
+            password = 'Password123',
+            bio = 'Hi, I am Jane.')
+        self.user.username ='@janedoe'
+        self._assert_user_is_invalid()
+    
+    def test_username_must_start_with_at_symbol(self):
+        self.user.username ='johndoe'
+        self._assert_user_is_invalid()
+    
+    def test_username_must_contin_only_alphanumericals_after_at(self):
+        self.user.username ='@john!doe'
+        self._assert_user_is_invalid()
+
+    def test_username_must_contin_at_least_3_alphanumericals_after_at(self):
+        self.user.username ='@jo'
+        self._assert_user_is_invalid()
+
+    def test_username_may_contain_numbers(self):
+        self.user.username ='@j0hndoe2'
+        self._assert_user_is_valid()
+    
+    def test_username_must_contin_only_one_at(self):
+        self.user.username ='@@johndoe'
+        self._assert_user_is_invalid()
+
+    def _assert_user_is_valid(self):
+        try:
+            self.user.full_clean()
+        except (ValidationError):
+            self.fail('Test user should be valid.')
+
+    def _assert_user_is_invalid(self):
+        with self.assertRaises(ValidationError):
+            self.user.full_clean()
