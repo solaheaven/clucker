f2736d9cf0c58db4bd76003fce62f91c5a28a2f1
Salihah Alnahdi <salihah.alnahdi@kcl.ac.uk>
2022-10-09 22:51:55 +0100

    Configure admin interface for users.

--- microblogs/admin.py
+++ microblogs/admin.py
@@ -1,3 +1,11 @@
+"""Configuration of the admin interface for microblogs."""
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    """Configuration of the admin interface for microblogs."""
+    
+    list_display = [
+        'username', 'first_name', 'last_name', 'email', 'is_active',
+    ]
